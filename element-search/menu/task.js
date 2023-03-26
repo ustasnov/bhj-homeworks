82d7398ed4785ae8591349b79813089a0ca2b1c0
ustasnov <ustasnov@gmail.com>
2023-03-26 17:27:21 +0700

    Changes for task 2_1

---
+++ element-search/menu/task.js
@@ -0,0 +1,13 @@
+const subMenus = document.querySelectorAll("ul.menu.menu_sub");
+
+Array.from(document.getElementsByClassName("menu__link")).forEach(element => {
+  element.onclick = event => {
+    let subMenu = event.currentTarget.closest("li").querySelector("ul.menu.menu_sub");
+    if (subMenu !== null) {
+      subMenus.forEach(element => {
+        element === subMenu ? element.classList.toggle("menu_active") : element.classList.remove("menu_active");
+      });
+    }
+    return subMenu === null;
+  }
+});
