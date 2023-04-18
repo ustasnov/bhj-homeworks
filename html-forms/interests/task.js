53e40f8c93ee8d2fa1a60aac5130c1fbe23416f9
ustasnov <ustasnov@gmail.com>
2023-04-19 02:44:43 +0700

    Changes for task 3_2

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,37 +1,40 @@
 const checkBoxes = Array.from(document.querySelectorAll(".interest__check"));
+const rootElement = document.querySelector(".interests_main");
 
-function setParentsState(element) {
-  let _parent = element.closest(".interest");
-  if (_parent) {
-    const childElements = Array.from(_parent.querySelectorAll(".interest"));
-    let checkedCount = 0;
-    childElements.forEach(child => {
-      checkedCount += child.childs[0].childs[0].checked ? 1 : 0;
+function setParentsState() {
+  const interests = rootElement.querySelectorAll(".interest");
+  interests.forEach(interest => {
+    const checks = Array.from(interest.querySelectorAll(".interest__check"));
+    let checksLength = 0;
+    let checksCount = 0;
+    checks.forEach(check => {
+      if (check.checked) {
+        checksCount++;
+      } 
+      if (!check.indeterminate) {
+        checksLength++
+      }
     });
-    _input = _parent.childs[0].childs[0];
-    _input.indeterminate = false;
-    if (checkedCount === 0) {
-      _input.checked = false;
-    } else if (checkedCount < childElements.length) {
-      _input.indeterminate = true;
-    } else {
-      _input.checked = true;
+    checks[0].indeterminate = false;
+    if (checksCount === 0) {
+      checks[0].checked = false;
+    } else if (checksCount === checksLength) {
+      checks[0].checked = true;
+    } else if (checksCount < checksLength) {
+      checks[0].checked = false;
+      checks[0].indeterminate = true;
     }
-    _parent = _parent.closest(".interest");
-    if (_parent) {
-      setParentsState(_parent);
-    }
-  }
+  });
 }
 
 function checkBoxChangeHandler(event) {
   const check = event.currentTarget;
   const _parent = check.closest(".interest");
   Array.from(_parent.querySelectorAll(".interest__check"))
-    .forEach(element => { 
-      element.checked = check.checked 
+    .forEach(element => {
+      element.checked = check.checked
     });
-  setParentsState(_parent);
+  setParentsState();
 }
 
 checkBoxes.forEach(element => {
